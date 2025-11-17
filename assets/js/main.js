7fce9c9ae811fd6f71a5f90c7df7dc1aad9b8070
muhammadbahawal <muhammadbahawalofficial@gmail.com>
2025-11-17 23:22:18 +0500

    home page ready and responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,3 +124,26 @@ function closeModalFunc() {
     youtubeFrame.src = "";
   }, 300); // removes video after close animation
 }
+
+
+
+// faq section 
+  document.addEventListener('DOMContentLoaded', function() {
+            const faqItems = document.querySelectorAll('.faq-item');
+            
+            faqItems.forEach(item => {
+                const question = item.querySelector('.faq-question');
+                
+                question.addEventListener('click', () => {
+                    // Close all other FAQ items
+                    faqItems.forEach(otherItem => {
+                        if (otherItem !== item) {
+                            otherItem.classList.remove('active');
+                        }
+                    });
+                    
+                    // Toggle current FAQ item
+                    item.classList.toggle('active');
+                });
+            });
+        });
